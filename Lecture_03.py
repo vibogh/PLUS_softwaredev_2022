4c9793d5bc1425f722eaeb0b62a00341cb5f5b95
Bochenek Victor <s1089123@is.sbg.ac.at>
2022-03-28 11:28:42 +0200

    ExerciseLecture03

---
+++ Lecture_03.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+# a comment
+
+print(sys.path)
+help("modules")
+days_of_the_week = 7
+def say_hello(recipient):
+    print("Hello, world! Hello {} ".format(recipient))
+    return recipient
